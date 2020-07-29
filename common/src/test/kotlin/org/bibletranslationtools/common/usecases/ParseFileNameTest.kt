e52a68a41661998c85024674605bac119e0e9d9f
mxaln <murmax82@gmail.com>
2020-07-29 11:53:47 -0400

    Refactored tests code

--- common/src/test/kotlin/org/bibletranslationtools/common/usecases/ParseFileNameTest.kt
+++ common/src/test/kotlin/org/bibletranslationtools/common/usecases/ParseFileNameTest.kt
@@ -11,157 +11,149 @@ import java.lang.IllegalArgumentException
 
 class ParseFileNameTest {
 
-    private val file1 = File("en_ot_ulb_b01_gen_c01_v01_t01.wav")
-    private val file2 = File("EN_ULB_B01_GEN_C01_V01_T01.wav")
-    private val file3 = File("test.wav")
-    private val file4 = File("en_ulb_gen_c02_chunk.tr")
-    private val file5 = File("en_ulb_gen_low_verse.mp3")
-    private val file6 = File("en_udb_gen.tr")
-    private val file7 = File("en_ulb.wav")
-
-    private val fileData1 = FileData(
-        file1,
-        "en",
-        ResourceType.ULB,
-        "gen",
-        1,
-        null,
-        null,
-        Grouping.VERSE
-    )
-
-    private val fileData2 = FileData(
-        file2,
-        "en",
-        ResourceType.ULB,
-        "gen",
-        1,
-        null,
-        null,
-        Grouping.VERSE
-    )
-
-    private val fileData3 = FileData(file3)
-
-    private val fileData4 = FileData(
-        file4,
-        "en",
-        ResourceType.ULB,
-        "gen",
-        2,
-        null,
-        null,
-        Grouping.CHUNK
-    )
-
-    private val fileData5 = FileData(
-        file5,
-        "en",
-        ResourceType.ULB,
-        "gen",
-        null,
-        null,
-        MediaQuality.LOW,
-        Grouping.VERSE
-    )
-
-    private val fileData7 = FileData(
-        file7,
-        "en",
-        ResourceType.ULB
-    )
-
-    private val parser1 = ParseFileName(file1)
-    private val parser2 = ParseFileName(file2)
-    private val parser3 = ParseFileName(file3)
-    private val parser4 = ParseFileName(file4)
-    private val parser5 = ParseFileName(file5)
-    private val parser6 = ParseFileName(file6)
-    private val parser7 = ParseFileName(file7)
-
     private val subscriber = TestSubscriber<FileData>()
 
     @Test
     fun parseFileNameWithValidInfo() {
-        val result = parser1.parse().toFlowable()
+        val file = File("en_ot_ulb_b01_gen_c01_v01_t01.wav")
+        val expected = FileData(
+            file,
+            "en",
+            ResourceType.ULB,
+            "gen",
+            1,
+            null,
+            null,
+            Grouping.VERSE
+        )
+        val result = ParseFileName(file).parse().toFlowable()
         result.subscribe(subscriber)
 
         subscriber.assertComplete()
         subscriber.assertNoErrors()
-        subscriber.assertValue(fileData1)
-    }
-
-    @Test
-    fun parseFileNameUpperCaseWithValidInfo() {
-        val result = parser2.parse().toFlowable()
-        result.subscribe(subscriber)
-
-        subscriber.assertComplete()
-        subscriber.assertNoErrors()
-        subscriber.assertValue(fileData2)
+        subscriber.assertValue(expected)
     }
 
     @Test
     fun parseFileNameWithInvalidInfo() {
-        val result = parser3.parse().toFlowable()
+        val file = File("test.wav")
+        val expected = FileData(file)
+        val result = ParseFileName(file).parse().toFlowable()
         result.subscribe(subscriber)
 
         subscriber.assertComplete()
         subscriber.assertNoErrors()
-        subscriber.assertValue(fileData3)
+        subscriber.assertValue(expected)
+    }
+
+    @Test
+    fun parseFileNameUpperCaseWithValidInfo() {
+        val file = File("EN_ULB_B01_GEN_C01_V01_T01.wav")
+        val expected = FileData(
+            file,
+            "en",
+            ResourceType.ULB,
+            "gen",
+            1,
+            null,
+            null,
+            Grouping.VERSE
+        )
+        val result = ParseFileName(file).parse().toFlowable()
+        result.subscribe(subscriber)
+
+        subscriber.assertComplete()
+        subscriber.assertNoErrors()
+        subscriber.assertValue(expected)
     }
 
     @Test
     fun fileDataHasLanguage() {
-        val result = parser1.parse().toFlowable()
+        val file = File("en_ot_ulb_b01_gen_c01_v01_t01.wav")
+        val expected = FileData(
+            file,
+            "en",
+            ResourceType.ULB,
+            "gen",
+            1,
+            null,
+            null,
+            Grouping.VERSE
+        )
+        val result = ParseFileName(file).parse().toFlowable()
         result.subscribe(subscriber)
 
         subscriber.assertComplete()
         subscriber.assertNoErrors()
         subscriber.assertValue {
-            it.language == fileData1.language
+            it.language == expected.language
         }
     }
 
     @Test
     fun fileDataLanguageNullWithInvalidFileName() {
-        val result = parser3.parse().toFlowable()
+        val file = File("test.wav")
+        val expected = FileData(file)
+        val result = ParseFileName(file).parse().toFlowable()
         result.subscribe(subscriber)
 
         subscriber.assertComplete()
         subscriber.assertNoErrors()
         subscriber.assertValue {
-            it.language == fileData3.language
+            it.language == expected.language
         }
     }
 
     @Test
     fun fileDataHasGrouping() {
-        val result = parser4.parse().toFlowable()
+        val file = File("en_ulb_gen_c02_chunk.tr")
+        val expected = FileData(
+            file,
+            "en",
+            ResourceType.ULB,
+            "gen",
+            2,
+            null,
+            null,
+            Grouping.CHUNK
+        )
+        val result = ParseFileName(file).parse().toFlowable()
         result.subscribe(subscriber)
 
         subscriber.assertComplete()
         subscriber.assertNoErrors()
         subscriber.assertValue {
-            it.grouping == fileData4.grouping
+            it.grouping == expected.grouping
         }
     }
 
     @Test
     fun fileDataHasMediaQuality() {
-        val result = parser5.parse().toFlowable()
+        val file = File("en_ulb_gen_low_verse.mp3")
+        val expected = FileData(
+            file,
+            "en",
+            ResourceType.ULB,
+            "gen",
+            null,
+            null,
+            MediaQuality.LOW,
+            Grouping.VERSE
+        )
+        val result = ParseFileName(file).parse().toFlowable()
         result.subscribe(subscriber)
 
         subscriber.assertComplete()
         subscriber.assertNoErrors()
         subscriber.assertValue {
-            it.mediaQuality == fileData5.mediaQuality
+            it.mediaQuality == expected.mediaQuality
         }
     }
 
     @Test
     fun unsupportedResourceTypeThrowsException() {
-        val result = parser6.parse().toFlowable()
+        val file = File("en_udb_gen.tr")
+        val result = ParseFileName(file).parse().toFlowable()
         result.subscribe(subscriber)
 
         subscriber.assertError(IllegalArgumentException::class.java)
@@ -170,11 +162,17 @@ class ParseFileNameTest {
 
     @Test
     fun parseFileNameLanguageResourceOnly() {
-        val result = parser7.parse().toFlowable()
+        val file = File("en_ulb.wav")
+        val fileData = FileData(
+            file,
+            "en",
+            ResourceType.ULB
+        )
+        val result = ParseFileName(file).parse().toFlowable()
         result.subscribe(subscriber)
 
         subscriber.assertComplete()
         subscriber.assertNoErrors()
-        subscriber.assertValue(fileData7)
+        subscriber.assertValue(fileData)
     }
 }
