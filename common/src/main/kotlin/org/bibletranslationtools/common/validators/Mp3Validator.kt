259feb83f1522787854b92ce24abe45b7adc0e93
mxaln <murmax82@gmail.com>
2020-08-18 13:01:44 -0400

    Added jpg, mp3 and cue validators

--- common/src/main/kotlin/org/bibletranslationtools/common/validators/Mp3Validator.kt
+++ common/src/main/kotlin/org/bibletranslationtools/common/validators/Mp3Validator.kt
@@ -1,5 +1,19 @@
 package org.bibletranslationtools.common.validators
 
+import org.apache.tika.Tika
 import java.io.File
 
-class Mp3Validator(private val file: File) : Validator()
+class Mp3Validator(private val file: File) : Validator() {
+
+    companion object {
+        const val MP3_MIME_TYPE = "audio/mpeg"
+    }
+
+    override fun validate() {
+        val fileType = Tika().detect(file)
+
+        if (fileType != MP3_MIME_TYPE) {
+            throw IllegalArgumentException("Not a mp3 file")
+        }
+    }
+}
