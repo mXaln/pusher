9dec596ae45328382fd6076d14752efb9f0c78f7
mxaln <murmax82@gmail.com>
2020-08-20 12:16:37 -0400

    Importing files to the app, validating them and parsing file names

--- jvm/src/main/kotlin/org/bibletranslationtools/jvm/ui/main/MainViewModel.kt
+++ jvm/src/main/kotlin/org/bibletranslationtools/jvm/ui/main/MainViewModel.kt
@@ -1,12 +1,22 @@
 package org.bibletranslationtools.jvm.ui.main
 
+import com.github.thomasnield.rxkotlinfx.observeOnFx
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 import javafx.beans.property.SimpleListProperty
 import org.bibletranslationtools.common.data.Grouping
 import org.bibletranslationtools.common.data.MediaExtension
 import org.bibletranslationtools.common.data.MediaQuality
 import org.bibletranslationtools.common.data.ResourceType
+import org.bibletranslationtools.common.usecases.ParseFileName
+import org.bibletranslationtools.common.usecases.ValidateFile
+import org.bibletranslationtools.jvm.io.BooksReader
+import org.bibletranslationtools.jvm.io.LanguagesReader
 import org.bibletranslationtools.jvm.ui.FileDataItem
+import org.bibletranslationtools.mappers.FileDataMapper
 import tornadofx.*
+import java.io.File
+import java.text.MessageFormat
 
 class MainViewModel : ViewModel() {
 
@@ -19,4 +29,77 @@ class MainViewModel : ViewModel() {
     val mediaExtensions = MediaExtension.values().toList().toObservable()
     val mediaQualities = MediaQuality.values().toList().toObservable()
     val groupings = Grouping.values().toList().toObservable()
+
+    val snackBarObservable: PublishSubject<String> = PublishSubject.create()
+
+    init {
+        loadLanguages()
+        loadBooks()
+    }
+
+    fun onDropFiles(files: List<File>) {
+        files.forEach {
+            if (it.isDirectory) {
+                importFolder(it)
+            } else {
+                importFile(it)
+            }
+        }
+    }
+
+    private fun importFile(file: File) {
+        if (file.isDirectory) return
+        ValidateFile(file).validate()
+            .subscribeOn(Schedulers.io())
+            .observeOnFx()
+            .doOnError {
+                val notImportedText = MessageFormat.format(messages["notImported"], file.name)
+                snackBarObservable.onNext("$notImportedText ${it.message ?: ""}")
+            }
+            .toSingleDefault(true)
+            .onErrorReturnItem(false)
+            .subscribe { success ->
+                if (success) {
+                    parseFileName(file)
+                }
+            }
+    }
+
+    private fun importFolder(folder: File) {
+        if (!folder.isDirectory) return
+
+        folder.walk().forEach {
+            importFile(it)
+        }
+    }
+
+    private fun parseFileName(file: File) {
+        ParseFileName(file).parse()
+            .subscribeOn(Schedulers.io())
+            .observeOnFx()
+            .subscribe { fileData ->
+                val fileDataItem = FileDataMapper().fromEntity(fileData)
+                if (!fileDataList.contains(fileDataItem)) {
+                    fileDataList.add(fileDataItem)
+                }
+            }
+    }
+
+    private fun loadLanguages() {
+        LanguagesReader().read()
+            .subscribeOn(Schedulers.io())
+            .observeOnFx()
+            .subscribe { _languages ->
+                languages.addAll(_languages)
+            }
+    }
+
+    private fun loadBooks() {
+        BooksReader().read()
+            .subscribeOn(Schedulers.io())
+            .observeOnFx()
+            .subscribe { _books ->
+                books.addAll(_books)
+            }
+    }
 }
