1f9fee9fa6375718da2ee21ddc85d7668a121bff
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-02-12 10:09:19 -0500

    use rc lib for getting audio content

--- common/src/main/kotlin/org/bibletranslationtools/common/usecases/ProcessOratureFile.kt
+++ common/src/main/kotlin/org/bibletranslationtools/common/usecases/ProcessOratureFile.kt
@@ -1,40 +1,32 @@
 package org.bibletranslationtools.common.usecases
 
 import org.bibletranslationtools.common.data.MediaExtension
+import org.wycliffeassociates.resourcecontainer.ResourceContainer
 import java.io.File
-import java.util.zip.ZipEntry
-import java.util.zip.ZipFile
 
 class ProcessOratureFile(private val file: File) {
 
-    /**
-     * This method extracts audio files under a specific directory within the zip
-     * Regard Orature resource container for more detail
-     */
     fun extractAudio(): List<File> {
         val tempDir = createTempDir().apply { deleteOnExit() }
-        val zipFile = ZipFile(file)
 
-        zipFile.entries().iterator().forEach { entry ->
-            val isWav = File(entry.name).extension == MediaExtension.WAV.toString()
-            if (entry.name.startsWith("content") && isWav) {
-                extractEntry(entry, zipFile, tempDir)
+        ResourceContainer.load(file).use { rc ->
+            val content = rc.getProjectContent(extension=MediaExtension.WAV.toString())
+                    ?: return listOf()
+
+            content.streams.forEach { entry ->
+                val normalizedFileName = File(entry.key).name.replace("_meta", "")
+                val destFile = tempDir.resolve(normalizedFileName)
+                destFile.deleteOnExit()
+
+                entry.value.buffered().use { input ->
+                    destFile.outputStream().buffered().use { output ->
+                        output.write(input.readBytes())
+                    }
+                }
             }
         }
-        zipFile.close()
 
         return tempDir.listFiles()?.toList() ?: listOf()
-    }
-
-    private fun extractEntry(entry: ZipEntry, zipFile: ZipFile, directory: File) {
-        val fileEntry = File(entry.name)
-        val destFile = directory.resolve(fileEntry.name).apply { deleteOnExit() }
-
-        zipFile.getInputStream(entry).buffered().use { input ->
-            destFile.outputStream().buffered().use { output ->
-                output.write(input.readBytes())
-            }
-        }
     }
 
     companion object {
