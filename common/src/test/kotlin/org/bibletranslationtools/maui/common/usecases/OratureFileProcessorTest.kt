96f0e76ba5db015ca57f8114a158a5f8fda5898d
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-02-22 15:49:00 -0500

    update test Orature Processor

--- common/src/test/kotlin/org/bibletranslationtools/maui/common/usecases/OratureFileProcessorTest.kt
+++ common/src/test/kotlin/org/bibletranslationtools/maui/common/usecases/OratureFileProcessorTest.kt
@@ -1,11 +1,14 @@
 package org.bibletranslationtools.maui.common.usecases
 
+import org.bibletranslationtools.maui.common.data.FileResult
+import org.bibletranslationtools.maui.common.data.FileStatus
 import org.bibletranslationtools.maui.common.data.MediaExtension
 import org.bibletranslationtools.maui.common.fileprocessor.OratureFileProcessor
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.io.File
 import java.io.FileNotFoundException
+import java.util.*
 
 class OratureFileProcessorTest {
     private val oratureFileName = "orature_file.zip"
@@ -14,7 +17,8 @@ class OratureFileProcessorTest {
     @Test
     fun testExtractAudioFiles() {
         val extension = MediaExtension.WAV.toString()
-        val files = OratureFileProcessor().extractAudio(getOratureFile(), extension)
+        val oratureFile = getOratureFile()
+        val files = OratureFileProcessor().extractAudio(oratureFile, extension)
 
         assertEquals(expectedWavFiles, files.size)
 
@@ -29,6 +33,18 @@ class OratureFileProcessorTest {
                 it.chapter != null
             }
         }
+    }
+
+    @Test
+    fun testOratureProcessor() {
+        val oratureFile = getOratureFile()
+        val exportList = mutableListOf<FileResult>()
+        val queue: Queue<File> = LinkedList<File>()
+        val status = OratureFileProcessor().process(oratureFile, queue, exportList)
+
+        assertEquals(FileStatus.PROCESSED, status)
+        assertEquals(2, queue.size)
+        assertEquals(0, exportList.size)
     }
 
     private fun getOratureFile(): File {
