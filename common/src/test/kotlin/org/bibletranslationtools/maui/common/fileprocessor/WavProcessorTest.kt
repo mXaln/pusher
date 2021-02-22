9c176a0f2440cef11025921b1dbcc0d2c1de3e44
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-02-22 16:48:30 -0500

    add test for wav processor

--- common/src/test/kotlin/org/bibletranslationtools/maui/common/fileprocessor/WavProcessorTest.kt
+++ common/src/test/kotlin/org/bibletranslationtools/maui/common/fileprocessor/WavProcessorTest.kt
@@ -2,7 +2,9 @@ package org.bibletranslationtools.maui.common.fileprocessor
 
 import org.bibletranslationtools.maui.common.data.FileResult
 import org.bibletranslationtools.maui.common.data.FileStatus
+import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import java.io.File
 import java.io.FileNotFoundException
@@ -11,16 +13,38 @@ import java.util.LinkedList
 
 class WavProcessorTest {
     private val goodWavFile = "en_ulb_b41_mat_c01.wav"
+    lateinit var queue: Queue<File>
+    lateinit var resultList: MutableList<FileResult>
+
+    @Before
+    fun setUp() {
+        queue = LinkedList<File>()
+        resultList = mutableListOf<FileResult>()
+    }
+
+    @After
+    fun cleanUp() {
+        queue.clear()
+        resultList.clear()
+    }
 
     @Test
     fun testProcessGoodFile() {
         val file = getTestFile(goodWavFile)
-        val queue: Queue<File> = LinkedList<File>()
-        val resultList = mutableListOf<FileResult>()
         val status = WavProcessor().process(file, queue, resultList)
 
         assertEquals(FileStatus.PROCESSED, status)
         assertEquals(1, resultList.size)
+        assertEquals(0, queue.size)
+    }
+
+    @Test
+    fun testProcessBadFile() {
+        val file = createTempFile(suffix=".wav").apply { deleteOnExit() }
+        val status = WavProcessor().process(file, queue, resultList)
+
+        assertEquals(FileStatus.REJECTED, status)
+        assertEquals(0, resultList.size)
         assertEquals(0, queue.size)
     }
 
